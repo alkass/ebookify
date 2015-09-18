5e747f613e2e574dc365093d9b59962b80b298b5
Fadi Hanna Al-Kass <fadi.eliya@yahoo.com>
2015-09-17 22:13:06 -0400

    .

--- makemigrations.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import os
-from django.core.management import execute_from_command_line as execute
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ebookify.settings")
-    execute(["manage.py", "makemigrations"])
