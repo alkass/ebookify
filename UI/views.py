9c0d02f2322f59d79524002e7dc42f037bec4212
Fadi Hanna Al-Kass <fadi.hanna_al-kass.ctr@us.af.mil>
2015-09-18 15:23:06 -0400

    .

--- UI/views.py
+++ UI/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
 from pyqrcode import create as create_qrcode
+from os.path import join
 
 def get_qr_code_image(request, identification):
     qr_code_img = create_qrcode("%s/download/%s" % (settings.PUBLIC_HOST_ADDRESS, identification))
-    qr_file_path = "%s.png" % identification
+    qr_file_path = join(settings.STATICFILES_DIRS[0], "media", "qrcodes", "%s.png" % identification)
     qr_code_img.png(qr_file_path, scale=11, module_color=(68, 50, 102), background=(243, 235, 246))
     response = HttpResponse(file(qr_file_path, "rb").read())
     response["Content-Type"] = "image/png"
