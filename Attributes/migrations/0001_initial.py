de0567b804b393e99e7ee0dc4caf3317e7d6c005
Fadi Hanna Al-Kass <eliya.3@wright.edu>
2016-07-22 01:20:11 -0400

    .

--- Attributes/migrations/0001_initial.py
+++ Attributes/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import uuid
 
 
 class Migration(migrations.Migration):
@@ -16,11 +17,49 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('full_name', models.CharField(max_length=256, unique=True, null=True)),
                 ('brief', models.TextField(max_length=2000, null=True, blank=True)),
-                ('deprecated', models.BooleanField(default=False)),
+                ('discoverable', models.BooleanField(default=True, help_text=b'If checked off, all books associated with this author will be discoverable to clients')),
             ],
             options={
                 'verbose_name': 'Author',
                 'verbose_name_plural': 'Authors',
+            },
+        ),
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('identification', models.UUIDField(default=uuid.uuid4, null=True, verbose_name=b'Book ID')),
+                ('title', models.CharField(max_length=256, null=True)),
+                ('subtitle', models.CharField(max_length=256, null=True, blank=True)),
+                ('pdf_file', models.FileField(upload_to=b'/Users/fadihannaal-kass/Desktop/ebookify/database/books', null=True, verbose_name=b'PDF File', blank=True)),
+                ('epub_file', models.FileField(upload_to=b'/Users/fadihannaal-kass/Desktop/ebookify/database/books', null=True, verbose_name=b'EPUB File', blank=True)),
+                ('mobi_file', models.FileField(upload_to=b'/Users/fadihannaal-kass/Desktop/ebookify/database/books', null=True, verbose_name=b'MOBI File', blank=True)),
+                ('cover', models.ImageField(null=True, upload_to=b'/Users/fadihannaal-kass/Desktop/ebookify/database/covers', blank=True)),
+                ('description', models.TextField(max_length=5000, null=True, blank=True)),
+                ('num_pages', models.IntegerField(default=0, help_text=b"Leave at 0 if you don't want the number of pages to be shown", verbose_name=b'number of Pages')),
+                ('num_views', models.IntegerField(default=0, verbose_name=b'Views')),
+                ('num_downloads', models.IntegerField(default=0, verbose_name=b'Downloads')),
+                ('discoverable', models.BooleanField(default=True, help_text=b'Make this book discoverable to view and download')),
+                ('recommended', models.BooleanField(default=False, help_text=b'Recommend this book to your library visitors')),
+                ('authors', models.ManyToManyField(to='Attributes.Author')),
+            ],
+            options={
+                'verbose_name': 'Book',
+                'verbose_name_plural': 'Books',
+            },
+        ),
+        migrations.CreateModel(
+            name='BookFeedback',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('feedback', models.TextField(max_length=1000, null=True)),
+                ('feedback_date', models.DateTimeField(auto_now_add=True)),
+                ('discoverable', models.BooleanField(default=True, help_text=b'Make this comment visible in the book view page')),
+                ('book', models.ForeignKey(to='Attributes.Book')),
+            ],
+            options={
+                'verbose_name': 'Book Feedback',
+                'verbose_name_plural': 'Book Feedbacks',
             },
         ),
         migrations.CreateModel(
@@ -29,7 +68,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=256, unique=True, null=True)),
                 ('brief', models.TextField(max_length=2000, null=True, blank=True)),
-                ('deprecated', models.BooleanField(default=False)),
+                ('discoverable', models.BooleanField(default=True, help_text=b'If checked off, all books associated with this category will be discoverable to clients')),
             ],
             options={
                 'verbose_name': 'Category',
@@ -41,7 +80,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('full_name', models.CharField(max_length=256, unique=True, null=True)),
-                ('deprecated', models.BooleanField(default=False)),
+                ('discoverable', models.BooleanField(default=True, help_text=b'If checked off, all books associated with this contributor will be discoverable to clients')),
             ],
             options={
                 'verbose_name': 'Contributor',
@@ -53,11 +92,26 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=256, unique=True, null=True)),
-                ('deprecated', models.BooleanField(default=False)),
+                ('discoverable', models.BooleanField(default=True, help_text=b'If checked off, all books associated with this language will be discoverable to clients')),
             ],
             options={
                 'verbose_name': 'Language',
                 'verbose_name_plural': 'Languages',
             },
+        ),
+        migrations.AddField(
+            model_name='book',
+            name='categories',
+            field=models.ManyToManyField(to='Attributes.Category'),
+        ),
+        migrations.AddField(
+            model_name='book',
+            name='contributors',
+            field=models.ManyToManyField(to='Attributes.Contributor', blank=True),
+        ),
+        migrations.AddField(
+            model_name='book',
+            name='language',
+            field=models.ForeignKey(to='Attributes.Language', null=True),
         ),
     ]
